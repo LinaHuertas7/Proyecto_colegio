646373766b109b0163136fa778e6e365aaded0d4
Lina Huertas <linamarcelahuertas7@gmail.com>
2022-07-18 22:06:15 -0500

    Agrego controller

---
+++ controllers/error.php
@@ -0,0 +1,8 @@
+<?
+
+class Error{
+    
+    function __construct(){
+        echo "<h1>Error al cargar el recurso</h1>";
+    }
+}
---
+++ controllers/main.php
@@ -0,0 +1,9 @@
+<?php
+
+class Main {
+    function __construct() {
+        echo "<h1>Nuevo controlador Main</h1>";
+    }
+
+}
+?>
