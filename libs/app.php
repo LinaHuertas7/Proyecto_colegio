0bd083474dff07f2e3a03b9145236ac9167c3ded
Lina Huertas <linamarcelahuertas7@gmail.com>
2022-07-18 22:02:42 -0500

    Controller

--- libs/app.php
+++ libs/app.php
@@ -1,4 +1,5 @@
 <?
+require_once "controllers/error.php";
 
 class App
 {
@@ -9,5 +10,19 @@ class App
         $url = $_GET["url"];
         $url = rtrim($url, "/");
         $url = explode("/", $url);
+
+        //var_dump($url);
+        $archivoController = "controllers/" . $url[0] . ".php";
+
+        if (file_exists($archivoController)) {
+            require_once $archivoController;
+            $controller = new $url[0];
+            if(isset($url[1])){
+                $controller->{$url[1]}();
+            }
+        
+        } else {
+            $controller = new Error();
+        }
     }
 }
